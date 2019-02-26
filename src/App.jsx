7ccca645d33c34fc780555bfd8679b00dfd699e2
winni <499207247@qq.com>
2019-02-26 13:26:44 +0800

    'antd环境搭建完成'

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
 /**
  * Created by Asus on 2019/2/26.
  */
+
+import React ,{Component} from 'react';
+import {render} from 'react-dom'
+import {Button,message} from 'antd'
+import Login from './pages/login'
+import Admin from './pages/admin'
+
+
+export default class App extends Component{
+    handleClick=()=>{
+        message.info('点击按钮',1);
+    }
+    render(){
+        return(
+            <div>
+                <Button onClick={this.handleClick} type='primary'>按钮</Button>
+            </div>
+        )
+    }
+}
